db381e122c72ed6f268c6ba837c4c535a6bec93a
trishaSolanki <141935315+trishaSolanki@users.noreply.github.com>
2023-11-30 18:34:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function volume_sphere() {
-    //Write your code here
-  
-} 
+  // Get the radius value entered in the form
+  const radius = parseFloat(document.getElementById("radius").value);
+
+  // Calculate the volume of the sphere using the formula
+  const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+
+  // Display the calculated volume in the volume field
+  document.getElementById("volume").value = volume.toFixed(2); // Round to 2 decimal places
+}
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
